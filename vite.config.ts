d3937ec89490b5c79bfa02eb732cd010d68ef3be
ellaidhurai n <ellaidhurai.97@gmail.com>
2023-07-16 19:00:36 +0530

    vite added

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,12 @@ import svgrPlugin from "vite-plugin-svgr";
 
 export default defineConfig({
 
-
   // Output directory for the production build
   build: {
     outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
   },
-
-  // Development server configuration
-  server: {
-    host: 'localhost',
-    port: 3000, 
-  },
-
+  
   // Configure plugins
   plugins: [
     // Add any additional plugins you may need
